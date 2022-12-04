1bfe2811c38d357229b16eccc206a91c4ca4e3e8
Iryna <batsianiryna@gmail.com>
2022-12-04 20:48:10 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задает массив из 8 элементов и выводит их на экран
+
+Console.Clear();
+Console.Write("Введите первое число массива: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число массива: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите третье число массива: ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите четвертое число массива: ");
+double n4 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите пятое число массива: ");
+double n5 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите шестое число массива: ");
+double n6 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите седьмое число массива: ");
+double n7 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите восьмое число массива: ");
+double n8 = Convert.ToDouble(Console.ReadLine());
+
+
+Console.WriteLine($"Результат: [{n1}, {n2}, {n3}, {n4}, {n5}, {n6}, {n7}, {n8}]");
