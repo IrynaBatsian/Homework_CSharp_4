77d24dbc9765031b82ccc1828aee6dea99cfd97d
Iryna <batsianiryna@gmail.com>
2022-12-04 19:57:26 +0300

    add task1 цикл, где одно число возводится в степень другого числа

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int result = a;
+while (i != b)
+{
+  result = result * a;
+  i++;
+}
+Console.WriteLine(result);
